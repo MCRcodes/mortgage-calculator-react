850970c85102d5205a25af5d391095a6dc83fd6c
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2019-08-14 20:10:48 +0100

    June 2019 - extra

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Button = ({
   type,
   handleOnclick,
   label,
 }) => <button type={type} onClick={handleOnclick}>{label}</button>;
+
+Button.propTypes = {
+  type: PropTypes.string.isRequired,
+  handleOnclick: PropTypes.func,
+  label: PropTypes.string,
+};
+
+Button.defaultProps = {
+  handleOnclick: () => {},
+  label: 'Cool button',
+};
 
 export default Button;
